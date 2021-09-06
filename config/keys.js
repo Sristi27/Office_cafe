5078245dc9145b17f934cac7d2d02a9131fe99c4
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-09-06 19:06:41 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports={
 
     SENDGRID_API:'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',
     MONGO_URI:'mongodb+srv://sristi27:270520@cluster0.hwmrk.mongodb.net/user?retryWrites=true&w=majority',
-    STRIPE_ID:'sk_test_51IpczwSH2HRN2FzsEDNbqB8YgUfdScGZ4RwlycM9EaWeAuCDSqLBuGboLyTMD4Dsu6vdLltmmvxLcsSgAyMckUXs00bnZBYkZk'
+    STRIPE_ID:'pk_test_51I4BBrI7Rq4MvTfMGre90OlhjGhOU437gSTOsMPej95Fw5RlaAfrSxvZvOJBMca2hdALVeFIhRg4wZ5QNtRTJbiN00saYUUbK7'
 
 }
